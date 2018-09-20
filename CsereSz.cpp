19944ac9d4b4cdc0b4c48f781d2586bf7fc1499a
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2018-09-20 21:27:36 +0200

    Add files via upload

---
+++ CsereSz.cpp
@@ -0,0 +1,31 @@
+#include"std_lib_facilities.h"
+int main()
+{
+	int a;
+	int b;	
+	cout<<"Adja meg az a szamot:";	
+	cin>>a;
+	cout<<"Adja meg az b szamot:";
+	cin>>b;
+	if(a==0 || b==0)
+	{
+		cout<<"A csere 0 szam eseten nem vegrehajthato"<<"\n";
+	}	
+	else if(a>b) 	
+	{		
+		b=b*a;
+		a=b/a;
+		b=b/a;
+		cout<<"a: "<< a <<"\n";
+		cout<<"b: "<< b << "\n";
+	}
+	else if(a<=b) 	
+	{		
+		a=a*b;
+		b=a/b;
+		a=a/b;
+		cout<<"a: "<< a <<"\n";
+		cout<<"b: "<< b << "\n";
+	}
+}
+	
